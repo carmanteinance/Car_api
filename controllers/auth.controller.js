47b66ae1f7ecc2e6c829567ff597b302bbb0bf3e
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2019-03-26 16:49:41 +0100

    register controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,23 @@ const User = require ('../models/user.model');
 const passport = require('passport');
 
 //Feature REGISTER
+module.exports.register = (res, req, next) => {
 
+    const {email} = req.body;
+
+    User.findOne({ email: email })
+        .then (user => {
+
+            if(user){
+                throw createError( 409, 'Upss this email is already registered. Are you? Go to Login')
+            }else {
+                return new User(req.body).save();
+            }
+        })
+
+        .then(user => res.status(201).json(user))
+        .catch(next)
+}
 
 //Feature LOGIN
 
