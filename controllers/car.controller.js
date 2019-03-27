5e25102f5a224c404c9559e18655ce472821c6af
Nerea <nereaparicio@gmail.com>
2019-03-27 20:49:50 +0100

    Feature Car-Controller. Working

---
+++ controllers/car.controller.js
@@ -0,0 +1,29 @@
+const createError = require('http-errors');
+const Car = require ('../models/car.model');
+
+
+module.exports.list = (req, res, next) =>{
+
+
+
+}
+
+module.exports.addCar = (req, res, next) =>{
+
+  
+
+}
+
+
+module.exports.doEdit = (req, res, next) =>{
+
+  
+
+}
+
+
+module.exports.delete = (req, res, next) =>{
+
+  
+
+}
