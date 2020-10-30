7f9d0ad826d593c70f2757ab2815711dfd316ee3
Nahv23 <nereaparicio@gmail.com>
2020-10-30 20:02:25 +0100

    solve (node:14876) DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. and (node:7452) DeprecationWarning: collection.ensureIndex is deprecated. Added eslint configuration too

--- configs/db.config.js
+++ configs/db.config.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const DB_NAME = 'Car-Maintenance'
 const MONGODB_URI = `mongodb://localhost:27017/${DB_NAME}`;
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.info(`Connected to the database: ${MONGODB_URI}`)
     })
     .catch(error => {
         console.error('Database connection error:', error);
     });
+
+mongoose.set('useCreateIndex', true);
